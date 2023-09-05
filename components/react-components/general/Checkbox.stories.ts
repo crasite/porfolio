a201f111ac63c5910afcf81a093e6db8670c37de
Thanu Poptiphueng <tpcrasite@gmail.com>
2023-09-05 11:36:15 +0700

    feat: remove id from checkbox

--- components/react-components/general/Checkbox.stories.ts
+++ components/react-components/general/Checkbox.stories.ts
@@ -16,7 +16,6 @@ export default {
   },
   args: {
     label: "Checkbox",
-    id: "cb",
   },
 };
 
--- components/react-components/general/Checkbox.tsx
+++ components/react-components/general/Checkbox.tsx
@@ -1,17 +1,19 @@
-import React, { forwardRef } from "react";
+import React, { forwardRef, useId } from "react";
 import { cn } from "../utils";
 
 export default forwardRef<HTMLInputElement, Props>(
   ({ className, label, ...rest }, ref) => {
+    const id = useId();
     return (
       <div className={cn("flex items-center gap-2", className)}>
         <input
           type="checkbox"
+          id={id}
           {...rest}
           ref={ref}
           className="border-gray text-primary accent-primary h-4 w-4 rounded"
         />
-        <label htmlFor={rest.id}>{label}</label>
+        <label htmlFor={id}>{label}</label>
       </div>
     );
   },
